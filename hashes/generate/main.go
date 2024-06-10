312dd99b38963b4ae6f36642564180ff725426ba
xypwn <54681180+xypwn@users.noreply.github.com>
2024-06-10 19:28:13 +0200

    Include HW12Dev's hash list

--- hashes/generate/main.go
+++ hashes/generate/main.go
@@ -9,8 +9,9 @@ import (
 	"strings"
 )
 
-const prolog = `// Copied from Hellextractor by Xaymar (https://github.com/Xaymar/Hellextractor)
-// as well as from hd2-name-db by DTZxPorter (https://github.com/dtzxporter/hd2-name-db)
+const prolog = `// Copied from Hellextractor by Xaymar (https://github.com/Xaymar/Hellextractor),
+// hd2-name-db by DTZxPorter (https://github.com/dtzxporter/hd2-name-db) and
+// Helldivers 2 Hashlist by HW12Dev (https://github.com/HW12Dev/Helldivers2-Hashlist).
 // This is a list of all known file type and file path strings.
 // Their respective hashes will be generated automatically.
 `
@@ -43,6 +44,14 @@ func main() {
 		os.Exit(1)
 	}
 	if err := appendHTTPFile(&strs, "https://raw.githubusercontent.com/Xaymar/Hellextractor/root/types.txt", nil); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	// This one includes a lot of garbage (probably a lot is scraped from strings), but it does improve hash coverage by a few percent.
+	if err := appendHTTPFile(&strs, "https://raw.githubusercontent.com/HW12Dev/Helldivers2-Hashlist/main/hashlist.txt", func(s string) string {
+		lhs, _, _ := strings.Cut(s, ".")
+		return lhs
+	}); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
