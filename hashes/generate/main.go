3799ccd25cde32b593d264fbbd4aa521fdd25211
Ryan Sims <ryanjsims@arizona.edu>
2024-12-30 12:13:02 -0700

    Add cracked hashes to hashlist generator

--- hashes/generate/main.go
+++ hashes/generate/main.go
@@ -62,6 +62,10 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	if err := appendHTTPFile(&strs, "https://raw.githubusercontent.com/ryanjsims/filediver/hashes/hashes/cracked.txt", nil); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
 	out, err := os.Create("hashes.txt")
 	if err != nil {
