8e68dc2536d2ee7d9ea92974418ee2272d417d27
Ryan Sims <ryanjsims@arizona.edu>
2026-04-28 15:28:15 -0700

    Looks like u32 formats also got changed

--- stingray/unit/unit.go
+++ stingray/unit/unit.go
@@ -208,10 +208,10 @@ const (
 	FormatVec3F                    MeshLayoutItemFormat = 2
 	FormatVec4F                    MeshLayoutItemFormat = 3
 	FormatS32                      MeshLayoutItemFormat = 4
-	FormatU32                      MeshLayoutItemFormat = 17
-	FormatVec2U32                  MeshLayoutItemFormat = 18
-	FormatVec3U32                  MeshLayoutItemFormat = 19
-	FormatVec4U32                  MeshLayoutItemFormat = 20
+	FormatU32                      MeshLayoutItemFormat = 21
+	FormatVec2U32                  MeshLayoutItemFormat = 22
+	FormatVec3U32                  MeshLayoutItemFormat = 23
+	FormatVec4U32                  MeshLayoutItemFormat = 24
 	FormatS8                       MeshLayoutItemFormat = 25
 	FormatVec2S8                   MeshLayoutItemFormat = 26
 	FormatVec3S8                   MeshLayoutItemFormat = 27
