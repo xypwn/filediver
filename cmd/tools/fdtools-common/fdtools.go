468f4834ed6a2673c71d15e46c3e276222d1f6f9
xypwn <54681180+xypwn@users.noreply.github.com>
2026-07-24 06:11:01 +0200

    Add fdtools-common (common filediver tool lib)

---
+++ cmd/tools/fdtools-common/fdtools.go
@@ -0,0 +1,65 @@
+// Common functionality for extra Filediver tools.
+package fdtools
+
+import (
+	"context"
+	"os"
+
+	"github.com/hellflame/argparse"
+	"github.com/jwalton/go-supportscolor"
+	"github.com/xypwn/filediver/app"
+	"github.com/xypwn/filediver/hashes"
+	"github.com/xypwn/filediver/stingray"
+)
+
+// Initializes the tool with a printer and app with game files and
+// adds a --gamedir (-a) argument, then executes the arg parser.
+//
+// This function is very impure: It will invoke the printer and may
+// exit the entire program. This is because it is solely intended
+// for standalone tools at the moment.
+//
+// Simply shuts down the program on failure (calls os.Exit(1) via prt.Fatalf).
+// Calls os.Exit(0) if --help (-h) is passed.
+func Init(argp *argparse.Parser) (prt app.Printer, a *app.App) {
+	prt = app.NewConsolePrinter(
+		supportscolor.Stderr().SupportsColor,
+		os.Stderr,
+		os.Stderr,
+	)
+
+	optGameDir := argp.String("g", "gamedir", nil)
+	if err := argp.Parse(nil); err != nil {
+		if err == argparse.BreakAfterHelpError {
+			os.Exit(0)
+		} else {
+			prt.Fatalf("%v", err)
+		}
+	}
+
+	var gameDir string
+	if *optGameDir == "" {
+		var err error
+		gameDir, err = app.DetectGameDir()
+		if err == nil {
+			prt.Infof("Using game found at: \"%v\"", gameDir)
+		} else {
+			prt.Errorf("Helldivers 2 Steam installation path not found: %v", err)
+			prt.Fatalf("Unable to detect game install directory. Please specify the game directory manually using the '-g' option.")
+		}
+	} else {
+		gameDir = *optGameDir
+	}
+
+	ctx := context.Background() // no need to exit cleanly since we're only reading
+	knownHashes := app.ParseHashes(hashes.Hashes)
+	knownThinHashes := app.ParseHashes(hashes.ThinHashes)
+	a, err := app.OpenGameDir(ctx, gameDir, knownHashes, knownThinHashes, stingray.ThinHash{}, func(curr, total int) {
+		prt.Statusf("Reading metadata %.0f%%", float64(curr)/float64(total)*100)
+	})
+	if err != nil {
+		prt.Fatalf("Error opening game dir: %v", err)
+	}
+	prt.NoStatus()
+	return
+}
