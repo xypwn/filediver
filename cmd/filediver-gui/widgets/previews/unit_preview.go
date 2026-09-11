b066b095b73552731ec7806ec2cf83bf264b5e94
Ryan Sims <ryanjsims@arizona.edu>
2026-09-11 13:19:47 -0700

    Allow models to be repositioned by holding shift while click-dragging

--- cmd/filediver-gui/widgets/previews/unit_preview.go
+++ cmd/filediver-gui/widgets/previews/unit_preview.go
@@ -115,6 +115,7 @@ type UnitPreviewState struct {
 	dbgObjUniforms unitPreviewUniforms
 
 	vfov         float32
+	modelPos     mgl32.Vec4
 	model        mgl32.Mat4
 	viewDistance float32
 	viewRotation mgl32.Vec2 // {yaw, pitch}
@@ -701,8 +702,20 @@ func UnitPreview(name string, pv *UnitPreviewState) {
 
 			if imgui.IsItemActive() {
 				md := io.MouseDelta()
-				pv.viewRotation = pv.viewRotation.Add(mgl32.Vec2{md.X, md.Y}.Mul(-0.01))
-				pv.viewRotation[1] = mgl32.Clamp(pv.viewRotation[1], -1.55, 1.55)
+				md4 := mgl32.Vec4{md.X, -md.Y, 0.0, 1.0}
+				if io.KeyShift() && md4.Vec2().LenSqr() > 0 {
+					_, viewPos, view, projection := pv.computeMVP(viewSize.X/viewSize.Y, false)
+					invProj := projection.Inv()
+					invModelView := pv.model.Inv().Mul4(view.Inv())
+					cameraPos := pv.model.Inv().Mul4x1(viewPos.Vec4(1.0))
+
+					invMouseDelta := invProj.Mul4x1(md4)
+					positionDelta := invModelView.Mul4x1(invMouseDelta.Vec2().Vec4(0.0, 1.0)).Sub(cameraPos)
+					pv.modelPos = pv.modelPos.Add(positionDelta.Mul(io.DeltaTime()))
+				} else {
+					pv.viewRotation = pv.viewRotation.Add(mgl32.Vec2{md.X, md.Y}.Mul(-0.01))
+					pv.viewRotation[1] = mgl32.Clamp(pv.viewRotation[1], -1.55, 1.55)
+				}
 			}
 			if imgui.IsItemDeactivated() && pv.autoZoomEnabled {
 				pv.doAutoZoomNextFrame = true
@@ -725,7 +738,9 @@ func UnitPreview(name string, pv *UnitPreviewState) {
 			gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 			normal, viewPosition, view, projection := pv.computeMVP(size.X/size.Y, true)
-			mvp := projection.Mul4(view).Mul4(pv.model)
+			position := pv.modelPos.Vec3()
+			translation := mgl32.Translate3D(position.X(), position.Y(), position.Z())
+			mvp := projection.Mul4(view).Mul4(pv.model.Mul4(translation))
 
 			// Draw object
 			gl.Enable(gl.DEPTH_TEST)
